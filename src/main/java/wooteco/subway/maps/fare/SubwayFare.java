fea06a08b719aca14a78499cff8e8e4a7a531adc
aegis <aegis1920@gmail.com>
2020-08-06 18:17:18 +0900

    feat: 노선별 추가 요금 중 가장 높은 추가요금만 내는 로직 작성 및 테스트

--- src/main/java/wooteco/subway/maps/fare/SubwayFare.java
+++ src/main/java/wooteco/subway/maps/fare/SubwayFare.java
@@ -1,28 +1,47 @@
 package wooteco.subway.maps.fare;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import wooteco.subway.maps.line.dto.LineResponse;
+
 public class SubwayFare {
 
     public static final Long DEFAULT_FARE = 1250L;
 
     private final int distance;
+    private final List<LineResponse> lineResponses;
 
-    public SubwayFare(int distance) {
+    public SubwayFare(int distance, List<LineResponse> lineResponses) {
         this.distance = distance;
+        this.lineResponses = lineResponses;
     }
 
-    public Long calculateSubwayFareByDistance() {
-        int plusFare = 0;
+    public Long calculateSubwayFare() {
+        int fareByDistance = 0;
+        int fareByLine = calculatePlusSubwayFare();
+
         if (distance <= 0) {
             throw new IllegalArgumentException(distance + " 거리는 요금 체계에 해당하지 않는 거리입니다!");
         }
         if (distance <= 10) {
-            return DEFAULT_FARE + plusFare;
+            return DEFAULT_FARE + fareByDistance + fareByLine;
         }
         if (distance <= 50) {
-            plusFare = (int) ((Math.ceil((distance - 1) / 5) + 1) * 100);
-            return DEFAULT_FARE + plusFare;
+            fareByDistance = (int) ((Math.ceil((distance - 1) / 5) + 1) * 100);
+            return DEFAULT_FARE + fareByDistance + fareByLine;
         }
-        plusFare = (int) ((Math.ceil((distance - 1) / 8) + 1) * 100);
-        return DEFAULT_FARE + plusFare;
+        fareByDistance = (int) ((Math.ceil((distance - 1) / 8) + 1) * 100);
+        return DEFAULT_FARE + fareByDistance + fareByLine;
+    }
+
+    private int calculatePlusSubwayFare() {
+        List<Integer> fares = lineResponses.stream()
+            .map(LineResponse::getExtraFare)
+            .collect(Collectors.toList());
+
+        return fares.stream()
+            .max(Comparator.comparingInt(o -> o))
+            .orElseThrow(IllegalAccessError::new);
     }
 }
--- src/test/java/wooteco/subway/maps/fare/domain/SubwayFareTest.java
+++ src/test/java/wooteco/subway/maps/fare/domain/SubwayFareTest.java
@@ -3,21 +3,84 @@ package wooteco.subway.maps.fare.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.google.common.collect.Lists;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import wooteco.subway.common.TestObjectUtils;
+import wooteco.subway.common.acceptance.AcceptanceTest;
 import wooteco.subway.maps.fare.SubwayFare;
+import wooteco.subway.maps.line.application.LineService;
+import wooteco.subway.maps.line.domain.Line;
+import wooteco.subway.maps.line.domain.LineStation;
+import wooteco.subway.maps.line.dto.LineResponse;
+import wooteco.subway.maps.map.domain.LineStationEdge;
+import wooteco.subway.maps.map.domain.SubwayPath;
+import wooteco.subway.maps.station.domain.Station;
 
-public class SubwayFareTest {
+public class SubwayFareTest extends AcceptanceTest {
+
+    @Autowired
+    private LineService lineService;
+
+    private Map<Long, Station> stations;
+    private List<Line> lines;
+
+    private SubwayPath subwayPath;
+
+    @BeforeEach
+    public void setUp() {
+        stations = new HashMap<>();
+        stations.put(1L, TestObjectUtils.createStation(1L, "교대역"));
+        stations.put(2L, TestObjectUtils.createStation(2L, "강남역"));
+        stations.put(3L, TestObjectUtils.createStation(3L, "양재역"));
+        stations.put(4L, TestObjectUtils.createStation(4L, "남부터미널역"));
+
+        Line line1 = TestObjectUtils.createLine(1L, "2호선", "GREEN", 0);
+        line1.addLineStation(new LineStation(1L, null, 0, 0));
+        LineStation lineStation2 = new LineStation(2L, 1L, 2, 2);
+        line1.addLineStation(new LineStation(2L, 1L, 2, 2));
+
+        Line line2 = TestObjectUtils.createLine(2L, "신분당선", "RED", 500);
+        line2.addLineStation(new LineStation(2L, null, 0, 0));
+        line2.addLineStation(new LineStation(3L, 2L, 2, 1));
+
+        Line line3 = TestObjectUtils.createLine(3L, "3호선", "ORANGE", 900);
+        line3.addLineStation(new LineStation(1L, null, 0, 0));
+        LineStation lineStation6 = new LineStation(4L, 1L, 1, 2);
+        LineStation lineStation7 = new LineStation(3L, 4L, 2, 2);
+        line3.addLineStation(lineStation6);
+        line3.addLineStation(lineStation7);
+
+        lines = Lists.newArrayList(line1, line2, line3);
+
+        List<LineStationEdge> lineStations = Lists.newArrayList(
+            new LineStationEdge(lineStation6, line3.getId()),
+            new LineStationEdge(lineStation7, line3.getId())
+        );
+        subwayPath = new SubwayPath(lineStations);
+    }
+
+    // TODO: 2020/08/06 다른 경우의 수도 할 것
 
     @DisplayName("0Km 이하일 경우 지하철 요금")
     @ParameterizedTest
     @ValueSource(ints = {-1, 0})
     void down0SubwayFareTest(int inputDistance) {
-        SubwayFare subwayFare = new SubwayFare(inputDistance);
+        System.out.println("hihi" + subwayPath.extractLineEdgeIds().toString());
 
-        assertThatThrownBy(subwayFare::calculateSubwayFareByDistance)
+        List<LineResponse> lineResponses = lineService
+            .findEdgeLineResponses(subwayPath.extractLineEdgeIds());
+
+        SubwayFare subwayFare = new SubwayFare(inputDistance, lineResponses);
+
+        assertThatThrownBy(subwayFare::calculateSubwayFare)
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -25,28 +88,35 @@ public class SubwayFareTest {
     @ParameterizedTest
     @ValueSource(ints = {1, 10})
     void defaultSubwayFareTest(int inputDistance) {
-        SubwayFare subwayFare = new SubwayFare(inputDistance);
+        List<LineResponse> lineResponses = lineService
+            .findEdgeLineResponses(subwayPath.extractLineEdgeIds());
 
-        assertThat(subwayFare.calculateSubwayFareByDistance()).isEqualTo(1250L);
+        SubwayFare subwayFare = new SubwayFare(inputDistance, lineResponses);
+
+        assertThat(subwayFare.calculateSubwayFare()).isEqualTo(2150L);
     }
 
     @DisplayName("이용 거리초과 시 추가운임(10km초과 ∼ 50km까지)일 경우 지하철 요금")
     @ParameterizedTest
-    @CsvSource({"11,1550", "50,2250"})
+    @CsvSource({"11,2450", "50,3150"})
     void between10to50SubwayFareTest(int inputDistance, Long expectedFare) {
-        SubwayFare subwayFare = new SubwayFare(inputDistance);
+        List<LineResponse> lineResponses = lineService
+            .findEdgeLineResponses(subwayPath.extractLineEdgeIds());
 
-        assertThat(subwayFare.calculateSubwayFareByDistance()).isEqualTo(expectedFare);
+        SubwayFare subwayFare = new SubwayFare(inputDistance, lineResponses);
+
+        assertThat(subwayFare.calculateSubwayFare()).isEqualTo(expectedFare);
     }
 
     @DisplayName("이용 거리초과 시 추가운임(50km초과)일 경우 지하철 요금")
     @ParameterizedTest
-    @CsvSource({"53,1950", "100,2550"})
+    @CsvSource({"53,2850", "100,3450"})
     void over50SubwayFareTest(int inputDistance, Long expectedFare) {
-        SubwayFare subwayFare = new SubwayFare(inputDistance);
+        List<LineResponse> lineResponses = lineService
+            .findEdgeLineResponses(subwayPath.extractLineEdgeIds());
 
-        assertThat(subwayFare.calculateSubwayFareByDistance()).isEqualTo(expectedFare);
+        SubwayFare subwayFare = new SubwayFare(inputDistance, lineResponses);
+
+        assertThat(subwayFare.calculateSubwayFare()).isEqualTo(expectedFare);
     }
-
-
 }
