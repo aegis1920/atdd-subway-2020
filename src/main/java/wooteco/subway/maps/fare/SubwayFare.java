f095985e34dee86a91548960c50d56d32455175d
aegis <aegis1920@gmail.com>
2020-08-06 15:53:30 +0900

    feat: SubwayFare 도메인 작성 및 테스트 (0Km 이하의 값)

--- src/main/java/wooteco/subway/maps/fare/SubwayFare.java
+++ src/main/java/wooteco/subway/maps/fare/SubwayFare.java
@@ -10,9 +10,11 @@ public class SubwayFare {
         this.distance = distance;
     }
 
-
     public Long calculateSubwayFareByDistance() {
         int plusFare = 0;
+        if (distance <= 0) {
+            throw new IllegalArgumentException(distance + " 거리는 요금 체계에 해당하지 않는 거리입니다!");
+        }
         if (distance <= 10) {
             return DEFAULT_FARE + plusFare;
         }
--- src/test/java/wooteco/subway/maps/fare/domain/SubwayFareTest.java
+++ src/test/java/wooteco/subway/maps/fare/domain/SubwayFareTest.java
@@ -1,6 +1,7 @@
 package wooteco.subway.maps.fare.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -9,6 +10,16 @@ import org.junit.jupiter.params.provider.ValueSource;
 import wooteco.subway.maps.fare.SubwayFare;
 
 public class SubwayFareTest {
+
+    @DisplayName("0Km 이하일 경우 지하철 요금")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void down0SubwayFareTest(int inputDistance) {
+        SubwayFare subwayFare = new SubwayFare(inputDistance);
+
+        assertThatThrownBy(subwayFare::calculateSubwayFareByDistance)
+            .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @DisplayName("기본운임(10Km 이내)일 경우 지하철 요금")
     @ParameterizedTest
