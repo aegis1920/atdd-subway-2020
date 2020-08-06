3fd4f64af6ce8083fb1a09787cb38dac5fcb7d13
aegis <aegis1920@gmail.com>
2020-08-06 15:45:27 +0900

    feat: SubwayFare 도메인 작성 및 테스트 (추가운임 요금, 10km초과 ∼ 50km까지)

--- src/main/java/wooteco/subway/maps/fare/SubwayFare.java
+++ src/main/java/wooteco/subway/maps/fare/SubwayFare.java
@@ -3,17 +3,25 @@ package wooteco.subway.maps.fare;
 public class SubwayFare {
 
     public static final Long DEFAULT_FARE = 1250L;
-    private final Long distance;
 
-    public SubwayFare(Long distance) {
+    private final int distance;
+
+    public SubwayFare(int distance) {
         this.distance = distance;
     }
 
 
     public Long calculateSubwayFareByDistance() {
+        int plusFare = 0;
+
         if (distance <= 10) {
-            return DEFAULT_FARE;
+            return DEFAULT_FARE + plusFare;
         }
+        if (distance <= 50) {
+            plusFare = (distance / 5) * 100;
+            return DEFAULT_FARE + plusFare;
+        }
+
         throw new IllegalArgumentException(distance + " 거리는 요금 체계에 해당하지 않는 거리입니다!");
     }
 }
--- src/test/java/wooteco/subway/maps/fare/domain/SubwayFareTest.java
+++ src/test/java/wooteco/subway/maps/fare/domain/SubwayFareTest.java
@@ -4,17 +4,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import wooteco.subway.maps.fare.SubwayFare;
 
 public class SubwayFareTest {
 
-    @DisplayName("기본운임(10Km 이내)일 경우 지하철 요금 ")
+    @DisplayName("기본운임(10Km 이내)일 경우 지하철 요금")
     @ParameterizedTest
-    @ValueSource(longs = {1L, 10L})
-    void defaultSubwayFareTest(Long inputDistance) {
+    @ValueSource(ints = {1, 10})
+    void defaultSubwayFareTest(int inputDistance) {
         SubwayFare subwayFare = new SubwayFare(inputDistance);
 
         assertThat(subwayFare.calculateSubwayFareByDistance()).isEqualTo(1250L);
     }
+
+    @DisplayName("이용 거리초과 시 추가운임(10km초과 ∼ 50km까지)일 경우 지하철 요금")
+    @ParameterizedTest
+    @CsvSource({"11,1450", "50,2250"})
+    void overSubwayFareTest(int inputDistance, Long expectedFare) {
+        SubwayFare subwayFare = new SubwayFare(inputDistance);
+
+        assertThat(subwayFare.calculateSubwayFareByDistance()).isEqualTo(expectedFare);
+    }
+
+
 }
